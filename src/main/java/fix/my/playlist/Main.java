c1aef5c78434dc09dc948e70e7143b8cab2771b5
drik <vallimagihendrik@gmail.com>
2023-05-20 00:14:59 +0300

    Change fixMyPlaylists signature to List<Playlist>. Populate List using reflection.

--- src/main/java/fix/my/playlist/Main.java
+++ src/main/java/fix/my/playlist/Main.java
@@ -1,12 +1,38 @@
 package fix.my.playlist;
 
 import fix.my.playlist.api.PlaylistUpdater;
+import fix.my.playlist.data.MyPlaylists;
+import fix.my.playlist.model.Playlist;
 
-import static fix.my.playlist.data.MyPlaylists.JUNGLE_DNB;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        PlaylistUpdater.fixMyPlaylist(JUNGLE_DNB);
+        var listOfPlaylists = getPlaylists();
+
+        PlaylistUpdater.fixMyPlaylists(listOfPlaylists);
+    }
+
+    public static List<Playlist> getPlaylists() {
+        List<Playlist> playlistList = new ArrayList<>();
+        Class<?> containingClass = MyPlaylists.class;
+
+        Field[] fields = containingClass.getDeclaredFields();
+        for (Field field : fields) {
+            if (field.getType() == Playlist.class) {
+                try {
+                    field.setAccessible(true);
+                    Playlist playlist = (Playlist) field.get(null);
+                    playlistList.add(playlist);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return playlistList;
     }
 }
