743d271698bae008ad6cc5f9065bdd1c017abd8a
drik <vallimagihendrik@gmail.com>
2023-05-19 17:58:02 +0300

    Break lines

--- src/main/java/fix/my/playlist/data/MyPlaylists.java
+++ src/main/java/fix/my/playlist/data/MyPlaylists.java
@@ -7,5 +7,9 @@ import java.util.Objects;
 
 public class MyPlaylists {
 
-    public static final Playlist JUNGLE_DNB = new Playlist("Jungle Drum & Bass", "JUNGLE IS MASSIVE!!!! Not on Spotify: Mask & Gang Related - Dictation ( Michael Caine remix ) jungle dnb", Objects.requireNonNull(PlaylistUpdater.class.getClassLoader().getResource("jungle_dnb_image.jpg")).getPath(), "4EAgTGzJpLvtDxQhqNgd0z");
+    public static final Playlist JUNGLE_DNB = new Playlist(
+        "Jungle Drum & Bass",
+        "JUNGLE IS MASSIVE!!!! Not on Spotify: Mask & Gang Related - Dictation ( Michael Caine remix ) jungle dnb",
+        Objects.requireNonNull(PlaylistUpdater.class.getClassLoader().getResource("jungle_dnb_image.jpg")).getPath(),
+        "4EAgTGzJpLvtDxQhqNgd0z");
 }
