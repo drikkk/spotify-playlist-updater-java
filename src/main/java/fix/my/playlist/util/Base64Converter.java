c66f496d1aa6461d19a4e4b8601cef25787bcb79
drik <vallimagihendrik@gmail.com>
2023-05-25 02:31:04 +0300

    Minor refactor

--- src/main/java/fix/my/playlist/util/Base64Converter.java
+++ src/main/java/fix/my/playlist/util/Base64Converter.java
@@ -3,12 +3,13 @@ package fix.my.playlist.util;
 import java.io.File;
 import java.nio.file.Files;
 import java.util.Base64;
+import java.util.Objects;
 
 public class Base64Converter {
 
-    public static String getBase64Image(File image) {
+    public static String getBase64Image(String imageName) {
         try {
-            var imageByteArray = Files.readAllBytes(image.toPath());
+            var imageByteArray = Files.readAllBytes(new File(Objects.requireNonNull(Base64Converter.class.getClassLoader().getResource(imageName)).getPath()).toPath());
             return Base64.getEncoder().encodeToString(imageByteArray);
         } catch (Exception e) {
             e.printStackTrace();
