e44e00a96a421ee48eb6c1951048308145a60e56
drik <vallimagihendrik@gmail.com>
2023-05-25 13:04:26 +0300

    Clean up class by extracting getPlaylists()

--- src/main/java/fix/my/playlist/Main.java
+++ src/main/java/fix/my/playlist/Main.java
@@ -1,36 +1,11 @@
 package fix.my.playlist;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import fix.my.playlist.api.ApiAdapter;
-import fix.my.playlist.model.Playlist;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.List;
+import fix.my.playlist.util.PlaylistHelper;
 
 public class Main {
-
-    private static final Logger log = LogManager.getLogger(Main.class);
-
     public static void main(String[] args) {
-        var playlistsFromJson = getPlaylists();
-        ApiAdapter.updatePlaylists(playlistsFromJson);
-    }
-
-    private static List<Playlist> getPlaylists() {
-        var objectMapper = new ObjectMapper();
-        var collectionType = objectMapper.getTypeFactory().constructCollectionType(List.class, Playlist.class);
-        var playlistJsonPathString = Paths.get(System.getProperty("user.dir"), "playlists", "playlists.json").toAbsolutePath().toString();
-        var jsonFile = new File(playlistJsonPathString);
-
-        try {
-            return objectMapper.readValue(jsonFile, collectionType);
-        } catch (IOException e) {
-            log.fatal("Error loading playlists.json");
-            throw new RuntimeException(e);
-        }
+        var myPlaylists = PlaylistHelper.getPlaylistsFromJson();
+        ApiAdapter.checkPlaylists(myPlaylists);
     }
 }
